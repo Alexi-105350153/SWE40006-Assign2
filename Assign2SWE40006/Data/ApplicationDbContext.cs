af6d46d960071af01e5e7ad131aaccb3e61913be
Alexi-105350153 <105350153@student.swin.edu.au>
2026-08-27 17:36:51 +1000

    Add a bunch of required files

---
+++ Assign2SWE40006/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assign2SWE40006.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ Assign2SWE40006/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assign2SWE40006.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
